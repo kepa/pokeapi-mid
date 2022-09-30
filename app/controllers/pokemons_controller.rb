eb9dc13fc7b18f31c8ef977458f81cacf69ce5b3
Lucas Falbo <95.lucas@gmail.com>
2022-09-30 14:27:22 -0300

    Add Poke module for support

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,4 +1,11 @@
 class PokemonsController < ApplicationController
   def info
+
+    @pokemon = PokeApi.get(pokemon: 'ditto')
+
+    respond_to do |format|
+        format.json { render :json => @pokemon.abilities }
+      end
+
   end
 end
