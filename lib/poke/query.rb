5cd5e26a85b0dc415f4de335a94310ef3b274ee3
Lucas Falbo <95.lucas@gmail.com>
2022-09-30 15:25:27 -0300

    Add main functionality to Poke::Query class

--- lib/poke/query.rb
+++ lib/poke/query.rb
@@ -4,9 +4,28 @@ require 'poke-api-v2'
 
 module Poke
   class Query
+    attr_reader :name
+
 
     def initialize(pokemon)
       @pokemon = PokeApi.get(pokemon: pokemon)
+      @name = pokemon.titlecase
+      @abilities = []
+    end
+
+    def abilities
+      @pokemon.abilities.each do |ability|
+        @abilities << ability.ability.name
+      end
+
+      @abilities.sort
+    end
+
+    def as_json(options={})
+      {
+        :name => self.name,
+        :abilities => self.abilities
+      }
     end
 
   end
--- spec/lib/poke_query_spec.rb
+++ spec/lib/poke_query_spec.rb
@@ -3,16 +3,48 @@ require_relative '../../lib/poke/query'
 
 RSpec.describe 'Querying PokeAPI' do
 
+  let(:pokemon_name) { Faker::Games::Pokemon.name.downcase }
+  let(:pokemon) {Poke::Query.new(pokemon_name)}
+
   describe 'basic query' do
-    let(:pokemon) { Faker::Games::Pokemon.name.downcase }
+
 
     it 'should return a valid pokemon if found' do
-      poke = Poke::Query.new(pokemon)
-      expect(poke).to be_truthy
+      expect(pokemon).to be_truthy
     end
 
     it 'should raise error if not found' do
       expect{Poke::Query.new('f')}.to raise_error(JSON::ParserError, "859: unexpected token at 'Not Found'")
+    end
+
+  end
+
+  describe '#name' do
+
+    it 'should return pokemon name' do
+      expect(pokemon.name).to eql(pokemon_name.titlecase)
+    end
+
+  end
+
+  describe '#abilities' do
+
+    it 'should return a list of abilities' do
+      expect(pokemon.abilities.count).to be > 0
+    end
+
+    it 'should return a list of abilities in alphabetical order' do
+      poke = Poke::Query.new('ditto')
+      expect(poke.abilities).to eql(["imposter", "limber"])
+    end
+
+  end
+
+  describe '#to_json' do
+
+    it 'should return a string that has name and abilities' do
+      expect(pokemon.to_json).to include('name')
+      expect(pokemon.to_json).to include('abilities')
     end
 
   end
